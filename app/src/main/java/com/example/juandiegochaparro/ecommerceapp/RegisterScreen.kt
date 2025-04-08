986cdbcf194e13e951ac90591968fc74d4c51c81
jjuandiego099 <jchaparro167@unab.edu.co>
2025-04-08 18:45:59 -0500

    Diseño RegisterScreen

---
+++ app/src/main/java/com/example/juandiegochaparro/ecommerceapp/RegisterScreen.kt
@@ -0,0 +1,129 @@
+package com.example.juandiegochaparro.ecommerceapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RegisterScreen() {
+
+    Scaffold { innerPading ->
+        Column(modifier = Modifier.padding(innerPading).fillMaxSize().padding(horizontal = 32.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Register",
+                modifier = Modifier.size(150.dp)
+            )
+            Text(
+                text = "Registrar Usuario",
+                fontSize = 24.sp, fontWeight = FontWeight.Bold,
+                color = Color(0xFFFF9900)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Nombre Completo") }, modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountBox,
+                        contentDescription = "Nombre Completo",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Correo Electronico") }, modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Correo Electronico",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Contraseña") }, modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Contraseña",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Confirmar Contraseña") }, modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Confirmar Contraseña",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors
+                    (containerColor = Color(0xFFFF9900)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "Registrarse",
+                    fontSize = 16.sp
+                )
+            }
+
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewRegisterScreen() {
+    RegisterScreen()
+
+}
